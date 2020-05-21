6566e57b7ea310e89196a17cb5a246d61c26891b
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2020-05-21 00:33:53 -0700

    Added image file extensions to the downloads using filetype

--- reddit_meme_downloader.py
+++ reddit_meme_downloader.py
@@ -8,19 +8,30 @@ import os
 
 import praw
 import requests
+import filetype
 
 # This is for convinience while developing
 def _object_attributes(object):
     pprint.pprint(vars(object))
 
 def download_meme(meme_item, save_dir):
-    # TODO: Figure out image file extensions/names based on URL
     image_url = meme_item.url
     filename = meme_item.id
+    filepath = save_dir + "/" + filename
 
-    with open(save_dir + "/" + filename, "wb") as download:
+    with open(filepath, "wb") as download:
         response = requests.get(image_url)
         download.write(response.content)
+
+        # Grab the filetype and make sure we have the right extension
+        type = filetype.guess(filepath)
+
+        if type is None:
+            print("Unable to determine image type!")
+            os.remove(filepath)
+            return
+
+        os.rename(filepath, filepath + "." + type.extension)
     
 BANNER_ART = """                                                                             
 ,------.           ,--.   ,--.,--.  ,--.      ,--.   ,--.                         
@@ -54,7 +65,8 @@ if __name__ == "__main__":
 
     reddit = praw.Reddit(site_name="user", user_agent="Reddit Meme Downloader")
 
-    os.mkdir(parsed.savedir)
+    if not os.path.isdir(parsed.savedir):
+        os.mkdir(parsed.savedir)
 
     # TODO: Add download time and number of items downloaded
     print("Downloading memes...")
@@ -62,3 +74,5 @@ if __name__ == "__main__":
         print("Downloading " + item.id + ".")
         download_meme(item, parsed.savedir)
         time.sleep(10)
+
+    print("Done!")
